e874744e44a599a281324c7a17c062ac4ba5ba70
Artyom Boyko <artyomboyko@gmail.com>
2015-11-16 23:55:49 +0200

    add basic js structure with jslint comments

---
+++ src/analytics/analytics/static/js/core.js
@@ -0,0 +1,16 @@
+/*
+ * core.js
+ * Root namespace module
+*/
+
+/*jslint           browser : true,   continue : true,
+  devel  : true,    indent : 2,       maxerr  : 50,
+  newcap : true,     nomen : true,   plusplus : true,
+  regexp : true,    sloppy : true,       vars : false,
+  white  : true
+*/
+/*global $, spa */
+
+var spa = (function () {
+    
+}());
