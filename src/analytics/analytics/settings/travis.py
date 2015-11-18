c3ddc18270dada6e6c943dd0670a1605d4bddad9
Artyom Boyko <artyomboyko@gmail.com>
2015-11-18 22:51:38 +0000

    add database settings to travis-ci configuration

--- src/analytics/analytics/settings/travis.py
+++ src/analytics/analytics/settings/travis.py
@@ -11,3 +11,12 @@ NOSE_ARGS = [
     '--cover-inclusive',
     '--cover-xml',
 ]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+DEBUG = True
