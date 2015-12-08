f447a7da7359321a84c29e174bb64d6fcd7bae67
Artyom Boyko <artyomboyko@gmail.com>
2015-12-08 12:21:58 +0200

    make celery use rabbitmq

---
+++ src/analytics/analytics/apps/core/tasks.py
@@ -0,0 +1,12 @@
+from celery.schedules import crontab
+from celery.task import PeriodicTask
+
+
+__all__ = ['ExampleTask']
+
+
+class ExampleTask(PeriodicTask):
+    run_every = crontab(minute=0, hour=0)
+
+    def run(self, *args, **kwargs):
+        print 'test'
